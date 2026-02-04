674458f9a3a23c62e22e2d67c16a5cf1213b7f82
695668461 <695668461@qq.com>
2026-02-05 01:27:12 +0800

    feat:静态分析完善。

--- app/version.h
+++ app/version.h
@@ -10,12 +10,12 @@
 #define VERSION_MINOR       1
 #define VERSION_PATCH       0
 
-#define GIT_HASH            "6f10f639"
+#define GIT_HASH            "b37daf43"
 #define GIT_BRANCH          "main"
-#define GIT_DIRTY           1
+#define GIT_DIRTY           0
 
-#define BUILD_TIME          "23:56:39"
-#define BUILD_DATE          "2026-02-04"
+#define BUILD_TIME          "01:18:48"
+#define BUILD_DATE          "2026-02-05"
 
 /* 完整版本字符串 */
 #define VERSION_STRING      "v0.1.0"
